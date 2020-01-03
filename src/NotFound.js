1ad6d979a48db9a5afdab434ba32aaca2380d879
Emil Elgaard <shivan@shivan.dk>
2020-01-04 00:08:47 +0100

    added page for 404

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,2 +1,14 @@
 import React from 'react';
 
+const NotFound = () => {
+
+    return (
+        <div className='not-found'>
+        <h2>Not results found</h2>
+        <h4>The search did not return any results, please try again.</h4>
+        </div>
+    );
+}
+
+
+export default NotFound;
